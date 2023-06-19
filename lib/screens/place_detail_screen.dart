79f53e0a73d32983ffa99565a955f9f5d235312e
Jimmy <jangho.chu@aiotholdings.com>
2023-06-19 14:31:17 +0900

    feat: add locationImage widget in place detail screen

--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_place_with_riverpod/models/place.dart';
+import 'package:flutter_place_with_riverpod/widgets/location_jnput.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key, required this.place});
 
   final Place place;
+
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longtitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$lat,$lng&key=$apiKey';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +21,48 @@ class PlaceDetailScreen extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          Image.file(place.image,
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity)
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.black54,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                    child: Text(
+                      place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
