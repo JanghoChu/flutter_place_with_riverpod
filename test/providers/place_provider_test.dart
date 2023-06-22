9d91a29f7a8896f72ca4eafd87d4b92030f32ede
Jimmy <jangho.chu@aiotholdings.com>
2023-06-22 18:51:39 +0900

    feat: add test scenario

--- test/providers/place_provider_test.dart
+++ test/providers/place_provider_test.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
 
-import 'package:flutter_place_with_riverpod/providers/place_provider.dart';
-import 'package:flutter_place_with_riverpod/providers/user_places.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_place_with_riverpod/providers/place_provider.dart';
+import 'package:flutter_place_with_riverpod/providers/user_places.dart';
 import 'package:flutter_place_with_riverpod/models/place.dart';
 
 class MockPlaceListener extends Mock {
-  void call(PlaceChangeNotifier? previous, PlaceChangeNotifier value);
+  void call(PlaceChangeNotifier? previous, PlaceChangeNotifier? value);
 }
 
 void main() {
@@ -36,8 +36,17 @@ void main() {
   );
 
   late PlaceChangeNotifier placeChangeNotifier;
+  late UserPlacesNotifier userPlacesNotifier;
   late ProviderContainer container;
-  late MockPlaceListener listener;
+  late MockPlaceListener mockPlacelistener;
+
+  late Place addedPlace;
+
+  listener(PlaceChangeNotifier? previous, PlaceChangeNotifier? value) {
+    if (value != null) {
+      addedPlace = value.place;
+    }
+  }
 
   group('place_change_notifier_test', () {
     setUp(() {
@@ -60,8 +69,15 @@ void main() {
 
   group('place_provider_test', () {
     setUp(() {
-      container = ProviderContainer();
-      listener = MockPlaceListener();
+      userPlacesNotifier = UserPlacesNotifier();
+      mockPlacelistener = MockPlaceListener();
+
+      container = ProviderContainer(
+        overrides: [
+          userPlacesProvider.overrideWith((ref) => userPlacesNotifier)
+        ],
+      );
+
       placeChangeNotifier = PlaceChangeNotifier(
           title: '',
           image: File.fromUri(Uri()),
@@ -72,31 +88,73 @@ void main() {
           ));
     });
 
-    test('notifies listener when place is changed', () async {
+    test(
+        'creates new placeChangeNotifier with last Place of userPlacesNotifier',
+        () async {
+      addTearDown(container.dispose);
+
+      userPlacesNotifier.addPlace(title, emptyImg, location);
+
+      expect(container.read(placeProvider).place.title, title);
+
+      userPlacesNotifier.addPlace(title2, emptyImg2, location2);
+
+      expect(container.read(placeProvider).place.title, title2);
+    });
+
+    test('notifies when user\'s places are added', () async {
       addTearDown(container.dispose);
 
       // 프로바이더를 관찰하고 값 변화를 검출
-      // container.listen<PlaceChangeNotifier>(
-      //   placeProvider,
-      //   listener.call,
-      //   fireImmediately: true,
-      // );
+      container.listen<PlaceChangeNotifier>(
+        placeProvider,
+        mockPlacelistener,
+        fireImmediately: true,
+      );
 
-      // 리스너는 placeChangeNotifier 을 기본값으로 호출
-      // verify(listener(null, placeChangeNotifier)).called(1);
-      // verifyNoMoreInteractions(listener);
+      // 최초 생성시 이벤트 발생
+      verify(mockPlacelistener(
+        any, // null은 any로 처리해야 오류가 발생하지 않음
+        argThat(isA<PlaceChangeNotifier>()),
+      )).called(1);
+      verifyNoMoreInteractions(mockPlacelistener);
 
-      // container
-      //     .read(userPlacesProvider.notifier)
-      //     .addPlace(title, emptyImg, location);
+      // userPlaces에 새로운 장소 추가
+      container
+          .read(userPlacesProvider.notifier)
+          .addPlace(title2, emptyImg2, location2);
 
-      // // Place를 변경
-      // container.read(placeProvider).changePlace(place);
+      // 장소 추가 후 placePovider를 한번 읽어야 이벤트를 listen함
+      container.read(placeProvider);
 
-      // 리스너는 [place]을 호출
-      // ** place객체의 equals method가 구현돼야 동일 객체로 인식
-      // verify(listener(placeChangeNotifier, placeChangeNotifier)).called(1);
-      // verifyNoMoreInteractions(listener);
-    }, skip: true);
+      // 새로운 장소 추가 시 이벤트 발생
+      verify(mockPlacelistener.call(
+        argThat(isA<PlaceChangeNotifier>()),
+        argThat(isA<PlaceChangeNotifier>()),
+      )).called(1);
+    });
+
+    test('updates Place when user\'s places are added', () async {
+      addTearDown(container.dispose);
+
+      // 프로바이더를 관찰하고 값 변화를 검출
+      container.listen<PlaceChangeNotifier>(
+        placeProvider,
+        listener.call,
+        fireImmediately: true,
+      );
+
+      expect(addedPlace.title, isEmpty);
+
+      // userPlaces에 새로운 장소 추가
+      container
+          .read(userPlacesProvider.notifier)
+          .addPlace(title2, emptyImg2, location2);
+
+      // 장소 추가 후 placePovider를 읽어야 이벤트를 listen함
+      container.read(placeProvider);
+
+      expect(addedPlace.title, title2);
+    });
   });
 }
