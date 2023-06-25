fef80981606ddea576b5ff4b2c7702536efe973d
Jimmy <jangho.chu@aiotholdings.com>
2023-06-25 23:53:38 +0900

    feat: add test bottom navi button

--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_place_with_riverpod/providers/user_places.dart';
 import 'package:flutter_place_with_riverpod/screens/add_place_screen.dart';
 import 'package:flutter_place_with_riverpod/widgets/place_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../models/place.dart';
 import '../providers/place_provider.dart';
 
 class PlacesScreen extends ConsumerWidget {
@@ -15,18 +18,32 @@ class PlacesScreen extends ConsumerWidget {
     final place = ref.watch(placeProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Places'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()),
-            ),
-          )
-        ],
-      ),
-      body: PlaceList(places: userPlaces),
-    );
+        appBar: AppBar(
+          title: const Text('Your Places'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => const AddPlaceScreen()),
+              ),
+            )
+          ],
+        ),
+        body: PlaceList(places: userPlaces),
+        bottomNavigationBar: ElevatedButton.icon(
+          icon: const Icon(Icons.add),
+          label: const Text('Add Place'),
+          onPressed: () async {
+            await ref.read(placeProvider).fetchPlace();
+            // ref.read(placeProvider).changePlace(Place(
+            //     title: "123",
+            //     image: File.fromUri(Uri()),
+            //     location: const PlaceLocation(
+            //       address: "address",
+            //       latitude: 123,
+            //       longtitude: 123,
+            //     )));
+          },
+        ));
   }
 }
